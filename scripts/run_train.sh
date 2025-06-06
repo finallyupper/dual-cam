38d5f9f37ee146c508ce5a24889469d860fea22e
finallyupper <dianaoh1021@gmail.com>
2025-06-06 09:26:42 +0900

    fix: add openimages30k configs

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -366,6 +366,57 @@ python main.py \
   --unfreeze_layer fc2 \
   --project resnet50-cub-0 
 
+# 3-1. inception_v3_cam
+python main.py \
+  --dataset_name CUB \
+  --architecture inception_v3 \
+  --wsol_method cam \
+  --method cam \
+  --experiment_name inception_v3_cam \
+  --wandb_name inception_v3_cam \
+  --root /data/wsol/bmvc2025/cub/inceptionv3 \
+  --large_feature_map TRUE \
+  --epoch 50 \
+  --batch_size 32 \
+  --lr_decay_frequency 15 \
+  --workers 4 \
+  --gpus 5 \
+  --lr 0.00224844746 \
+  --weight_decay 5.00E-04 \
+  --model_structure vanilla \
+  --project inceptionv3-cub-0 \
+  --eval_only \
+  --eval_checkpoint_type last \
+  --loc_threshold 0.05
+
+# 3-2. inception_v3_cam + ours
+
+# 3-3. inception_v3_gradcam
+python main.py \
+  --dataset_name CUB \
+  --architecture inception_v3 \
+  --wsol_method cam \
+  --method gradcam \
+  --experiment_name inceptionv3_gradcam \
+  --wandb_name inceptionv3_gradcam \
+  --root /data/wsol/bmvc2025/cub/inceptionv3 \
+  --large_feature_map TRUE \
+  --epoch 50 \
+  --batch_size 64 \
+  --lr_decay_frequency 15 \
+  --workers 4 \
+  --gpus 5 \
+  --lr 0.00224844746 \
+  --weight_decay 5.00E-04 \
+  --model_structure vanilla \
+  --project inceptionv3-cub-0 \
+  --eval_only \
+  --eval_checkpoint_type last \
+  --loc_threshold 0.05
+
+# 3-4. inception_v3_gradcam + ours
+
+
 # ---------------------------------------------------------------- #
 
 # cars
@@ -536,3 +587,258 @@ python main.py \
   --ft_ckpt /home/yoojinoh/recam-imagenet-gradcam/experiment/cars/resnet50/resnet50_gradcam/last_checkpoint.pth.tar \
   --unfreeze_layer fc2 \
   --project resnet50-cars-0 
+
+# -------------------------------------------------------------- #
+# OpenImages30K
+
+# 1-1. vgg16_cam 
+python main.py \
+  --dataset_name OpenImages \
+  --architecture vgg16 \
+  --wsol_method cam \
+  --method cam \
+  --experiment_name vgg16_cam \
+  --wandb_name vgg16_cam \
+  --root /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/ \
+  --large_feature_map FALSE \
+  --epoch 10 \
+  --batch_size 32 \
+  --lr_decay_frequency 3 \
+  --workers 4 \
+  --gpus 1 \
+  --lr 0.00034052401 \
+  --weight_decay 5.00E-04 \
+  --model_structure vanilla \
+  --project vgg16-openimages-0 
+
+# 1-2. vgg16_cam + ours
+python main.py \
+  --dataset_name OpenImages \
+  --architecture vgg16 \
+  --wsol_method cam \
+  --method cam \
+  --experiment_name vgg16_cam_ours \
+  --wandb_name vgg16_cam_ours \
+  --root /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/ \
+  --large_feature_map FALSE \
+  --epoch 10 \
+  --batch_size 32 \
+  --lr_decay_frequency 3 \
+  --workers 4 \
+  --gpus 1 \
+  --lr 0.0005 \
+  --weight_decay 5.00E-04 \
+  --model_structure b2 \
+  --ft_ckpt /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/vgg16/vgg16_cam/last_checkpoint.pth.tar \
+  --unfreeze_layer fc2 \
+  --project vgg16-openimages-0 
+
+# 1-3. vgg16_gradcam 
+python main.py \
+  --dataset_name OpenImages \
+  --architecture vgg16 \
+  --wsol_method cam \
+  --method gradcam \
+  --experiment_name vgg16_gradcam \
+  --wandb_name vgg16_gradcam \
+  --root /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/ \
+  --large_feature_map FALSE \
+  --epoch 10 \
+  --batch_size 32 \
+  --lr_decay_frequency 3 \
+  --workers 4 \
+  --gpus 1 \
+  --lr 0.00034052401 \
+  --weight_decay 5.00E-04 \
+  --model_structure vanilla \
+  --project vgg16-openimages-0 
+
+# 1-4. vgg16_gradcam + ours
+python main.py \
+  --dataset_name OpenImages \
+  --architecture vgg16 \
+  --wsol_method cam \
+  --method gradcam \
+  --experiment_name vgg16_gradcam_ours \
+  --wandb_name vgg16_gradcam_ours \
+  --root /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/ \
+  --large_feature_map FALSE \
+  --epoch 10 \
+  --batch_size 32 \
+  --lr_decay_frequency 3 \
+  --workers 4 \
+  --gpus 1 \
+  --lr 0.0005 \
+  --weight_decay 5.00E-04 \
+  --model_structure b2 \
+  --ft_ckpt /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/vgg16/vgg16_gradcam/last_checkpoint.pth.tar \
+  --unfreeze_layer classifier_2 \
+  --project vgg16-openimages-0
+
+# 2-1. resnet50_cam
+python main.py \
+  --dataset_name OpenImages \
+  --architecture resnet50 \
+  --wsol_method cam \
+  --method cam \
+  --experiment_name resnet50_cam \
+  --wandb_name resnet50_cam \
+  --root /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/ \
+  --large_feature_map FALSE \
+  --epoch 10 \
+  --batch_size 32 \
+  --lr_decay_frequency 3 \
+  --workers 4 \
+  --gpus 1 \
+  --lr 0.00107117324 \
+  --weight_decay 1.00E-04 \
+  --model_structure vanilla \
+  --project resnet50-openimages-0 
+
+# 2-2. resnet50_cam + ours
+python main.py \
+  --dataset_name OpenImages \
+  --architecture resnet50 \
+  --wsol_method cam \
+  --method gradcam \
+  --experiment_name resnet50_gradcam_ours \
+  --wandb_name resnet50_gradcam_ours \
+  --root /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/ \
+  --large_feature_map FALSE \
+  --epoch 10 \
+  --batch_size 32 \
+  --lr_decay_frequency 3 \
+  --workers 4 \
+  --gpus 1 \
+  --lr 0.02 \
+  --weight_decay 1.00E-04 \
+  --model_structure b2 \
+  --ft_ckpt /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/resnet50/resnet50_gradcam/last_checkpoint.pth.tar \
+  --unfreeze_layer fc2 \
+  --project resnet50-openimages-0
+
+# 2-3. resnet50_gradcam
+python main.py \
+  --dataset_name OpenImages \
+  --architecture resnet50 \
+  --wsol_method cam \
+  --method gradcam \
+  --experiment_name resnet50_gradcam \
+  --wandb_name resnet50_gradcam \
+  --root /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/ \
+  --large_feature_map FALSE \
+  --epoch 10 \
+  --batch_size 32 \
+  --lr_decay_frequency 3 \
+  --workers 4 \
+  --gpus 1 \
+  --lr 0.00107117324 \
+  --weight_decay 1.00E-04 \
+  --model_structure vanilla \
+  --project resnet50-openimages-0 
+
+# 2-4. resnet50_gradcam + ours (weight decay 5.00E-04)
+python main.py \
+  --dataset_name OpenImages \
+  --architecture resnet50 \
+  --wsol_method cam \
+  --method gradcam \
+  --experiment_name resnet50_gradcam_ours \
+  --wandb_name resnet50_gradcam_ours \
+  --root /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/ \
+  --large_feature_map FALSE \
+  --epoch 10 \
+  --batch_size 32 \
+  --lr_decay_frequency 3 \
+  --workers 4 \
+  --gpus 1 \
+  --lr 0.0005 \
+  --weight_decay 5.00E-04 \
+  --model_structure b2 \
+  --ft_ckpt /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/resnet50/resnet50_gradcam/last_checkpoint.pth.tar \
+  --unfreeze_layer fc2 \
+  --project resnet50-openimages-0
+
+# 3-1. inception_v3_cam
+python main.py \
+  --dataset_name OpenImages \
+  --architecture inception_v3 \
+  --wsol_method cam \
+  --method cam \
+  --experiment_name inception_v3_cam \
+  --wandb_name inception_v3_cam \
+  --root /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/ \
+  --large_feature_map TRUE \
+  --epoch 10 \
+  --batch_size 32 \
+  --lr_decay_frequency 3 \
+  --workers 4 \
+  --gpus 1 \
+  --lr 0.0003 \
+  --weight_decay 5.00E-04 \
+  --model_structure vanilla \
+  --project inceptionv3-openimages-0 
+
+# 3-2. inception_v3_cam + ours
+python main.py \
+  --dataset_name OpenImages \
+  --architecture inception_v3 \
+  --wsol_method cam \
+  --method cam \
+  --experiment_name inception_v3_cam \
+  --wandb_name inception_v3_cam \
+  --root /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/ \
+  --large_feature_map TRUE \
+  --epoch 10 \
+  --batch_size 32 \
+  --lr_decay_frequency 3 \
+  --workers 4 \
+  --gpus 1 \
+  --lr 0.0003 \
+  --weight_decay 5.00E-04 \
+  --model_structure b2 \
+  --ft_ckpt /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/inceptionv3/inception_v3_cam/last_checkpoint.pth.tar \
+  --unfreeze_layer SPG_A4_2 \
+  --project inceptionv3-openimages-0
+
+# 3-3. inceptionv3_gradcam
+python main.py \
+  --dataset_name OpenImages \
+  --architecture inception_v3 \
+  --wsol_method cam \
+  --method gradcam \
+  --experiment_name inceptionv3_gradcam \
+  --wandb_name inceptionv3_gradcam \
+  --root /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/ \
+  --large_feature_map TRUE \
+  --epoch 10 \
+  --batch_size 32 \
+  --lr_decay_frequency 3 \
+  --workers 4 \
+  --gpus 1 \
+  --lr 0.0003 \
+  --weight_decay 5.00E-04 \
+  --model_structure vanilla \
+  --project inceptionv3-openimages-0
+
+# 3-4. inceptionv3_gradcam + ours
+python main.py \
+  --dataset_name OpenImages \
+  --architecture inception_v3 \
+  --wsol_method cam \
+  --method gradcam \
+  --experiment_name inceptionv3_gradcam_ours \
+  --wandb_name inceptionv3_gradcam_ours \
+  --root /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/ \
+  --large_feature_map TRUE \
+  --epoch 10 \
+  --batch_size 32 \
+  --lr_decay_frequency 3 \
+  --workers 4 \
+  --gpus 1 \
+  --lr 0.0003 \
+  --weight_decay 5.00E-04 \
+  --model_structure b2 \
+  --ft_ckpt /home/yoojinoh/recam-imagenet-gradcam/experiment/openimages/inceptionv3/inception_v3_gradcam/last_checkpoint.pth.tar \
+  --unfreeze_layer fc2 \
+  --project inceptionv3-openimages-0
